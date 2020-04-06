bc967435d6b0a978b253ac05784fe0c17e4e10e5
Christian Clauss <cclauss@me.com>
2020-04-06 11:27:27 +0200

    lcc_letters_and_numbers["D"] = "History, General and Old World"

--- lcc_classifier.py
+++ lcc_classifier.py
@@ -18,7 +18,8 @@ with open(os.path.join(here, "lc_classifiers_letters_only.json")) as in_file:
 with open(os.path.join(here, "lc_classifiers_letters_and_numbers.json")) as in_file:
     lcc_letters_and_numbers = json.load(in_file)
 # Make corrections...
-lcc_letters_and_numbers["D"] = "World History and History of Europe"
+# "D": "World History and History of Europe" -->
+lcc_letters_and_numbers["D"] = "History, General and Old World"
 out_file = None
 
 

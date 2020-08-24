7b5f022ec2b5aed17acb4db15b721cab37a95381
Christian Clauss <cclauss@me.com>
2020-08-24 17:43:53 +0200

    Update openlibrary_simple_import.py

--- openlibrary_simple_import.py
+++ openlibrary_simple_import.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import requests
+
+# PART I: Gather data from OpenLibrary
 URL_FMT = "https://openlibrary.org/{key}.json"
 
 
@@ -12,15 +14,18 @@ key = la_grande_neige["authors"][0]["author"]["key"]  # '/authors/OL7440583A'
 fabienne_clauss = requests.get(URL_FMT.format(key=key)).json()
 print(f"{fabienne_clauss = }")
 
+# PART II: Import data into localhost
 """
 POST_URL = "http://localhost:8080/api/import/ia"
 print(requests.post(POST_URL, json=fabienne_clauss).text)
 print(requests.post(POST_URL, json=la_grande_neige).text)
 """
 
-NEW_URL = "https://openlibrary.org/api/new.json"
-fabienne_clauss.pop("created")
-fabienne_clauss.pop("last_modified")
-fabienne_clauss.pop("latest_revision")
+NEW_URL = "http://localhost:8080/api/new.json"
+for key in "created key last_modified latest_revision revision".split():
+    fabienne_clauss.pop(key)
+
+print(f"{fabienne_clauss = }")
+# fabienne_clauss = {'name': 'Fabienne Clauss', 'type': {'key': '/type/author'}}
 print(requests.post(NEW_URL, json=fabienne_clauss).text)
-# --> Permission Denied.
+print("Done.")
